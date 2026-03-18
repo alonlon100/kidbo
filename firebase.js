c6bf2709ca25535cdb34a4ea8a209eb76cd6bf96
alonlon100 <nvnvbnbbnbnb@gmail.com>
2026-03-18 18:08:55 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,40 +1,17 @@
-// firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import {
-getFirestore,
-collection,
-doc,
-setDoc,
-getDoc,
-getDocs,
-addDoc,
-deleteDoc
-}
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyBmpKk-tKwyvsB-g4TGqee9bVK1DMLdKEw",
-authDomain: "kidbo-f1dca.firebaseapp.com",
-projectId: "kidbo-f1dca",
-storageBucket: "kidbo-f1dca.appspot.com",
-messagingSenderId: "114331743526",
-appId: "1:114331743526:web:01d938deecaa106db1fde7"
+  apiKey: "AIzaSyBmpKk-tKwyvsB-g4TGqee9bVK1DMLdKEw",
+  authDomain: "kidbo-f1dca.firebaseapp.com",
+  projectId: "kidbo-f1dca",
+  storageBucket: "kidbo-f1dca.appspot.com",
+  messagingSenderId: "114331743526",
+  appId: "1:114331743526:web:fd203cea16504e43b1fde7"
 };
-
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export {
-db,
-collection,
-doc,
-setDoc,
-getDoc,
-getDocs,
-addDoc,
-deleteDoc
-};
+export const auth = getAuth(app);
+export const db = getFirestore(app);
